59acd7ddec811beddfaca388841199d74b1c61f9
NoonAk-N <noonakoud@gmail.com>
2023-08-02 12:58:53 +0200

    firsttt commit

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
+import { addGreeting, getGreetings } from './db.js'
 
-
-//start here
 import express from 'express';
-import {getGreetings, addGreeting} from './db.js'
-
 const app = express();
+
 app.use(express.json());
 const PORT = process.env.PORT || 3008;
 
-// get all the greetings
-//http://localhost:3008/api/greetings
-app.get('/api/greetings',  async(req,res) => {
+app.get('/api/greetings', async (req, res) => {
 
-const greetings = await getGreetings();
-console.log(greetings);
-res.json({
-    greetings 
-})
+    const greetings = await getGreetings();
+    console.log(greetings);
+    res.json({
+        greetings
+    })
 });
-app.listen(PORT, () =>console.log(`start on port : ${PORT}`) )
-//create a route to add a greeting  
 
-app.post('/api/greetings',(req, res) => {
+app.post('/api/greetings', async (req, res) => {
     const greeting = req.body.greeting;
     const language = req.body.language;
 
-//
-res.json({
-    greetings,
-    language
+    await addGreeting(language, greeting)
+
+    res.json({
+        status: 'success',
+        message: `Added a greeting for ${language},`
+
+    });
 });
 
 
-});
+app.listen(PORT, () => console.log(`started on port: ${PORT}`))
+
+console.log('start')
+
+
+
+
+
+//console.log('end')
